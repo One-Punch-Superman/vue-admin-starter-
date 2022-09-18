e1e09809fc288d98b35ffa82ee6ba12510a3a6f7
cmz <764213669@qq.com>
2022-09-18 22:49:03 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">{{ str }}</div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+const str = ref('11');
+onMounted(() => {
+  console.log();
+});
+</script>
+<style lang="scss" scoped>
+div {
+  height: 1500px;
+}
+</style>
