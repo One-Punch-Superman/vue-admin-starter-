9da6f93942410abef35666e1bba70c174243c77b
One-Punch-Superman <764213669@qq.com>
2023-05-08 21:18:04 +0800

    init

--- src/api/system.ts
+++ src/api/system.ts
@@ -3,3 +3,7 @@ import { get } from '@/utils/request';
 export function getUserList() {
   return get('/system/userList');
 }
+
+export function getUserList2() {
+  return get('/system/userList');
+}
